a5a7d16a57cf48a05c1501035b1aeca67fdcfa97
eallenop <e>
2024-10-15 11:17:51 +1300

    Set up the about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sequi perferendis soluta dolores consequuntur, aperiam fugit voluptatum ea incidunt rem, saepe unde veritatis quibusdam temporibus ut tempora nostrum commodi. Voluptate.</p>
+
+<style>
+
+</style>
