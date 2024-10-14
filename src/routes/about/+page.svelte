24aa3ed50a23b8b7d7611ce1a90e574da9eb8aae
eallenop <e>
2024-10-15 11:19:51 +1300

    Add the header component to the about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-
+    import Header from "$lib/Header.svelte"
 </script>
 
+<Header headingTitle="About us" />
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sequi perferendis soluta dolores consequuntur, aperiam fugit voluptatum ea incidunt rem, saepe unde veritatis quibusdam temporibus ut tempora nostrum commodi. Voluptate.</p>
 
 <style>
