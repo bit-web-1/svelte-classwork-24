b3002bf87dac325f71165749ba8816574c6d5a20
eallenop <e>
2024-10-15 10:49:13 +1300

    Fill in heading and nav content

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,9 +3,16 @@
 </script>
 
 <header>
-
+    <h1>My class work website</h1>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Contact</a></li>
+        </ul>
+    </nav>
 </header>
 
 <style>
-    
+
 </style>
