1c17f18b7c98a765ec496b2d2a46433128f90845
eallenop <e>
2024-10-15 11:32:25 +1300

    Add a contact page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -8,7 +8,7 @@
         <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
-            <li><a href="/">Contact</a></li>
+            <li><a href="/contact">Contact</a></li>
         </ul>
     </nav>
 </header>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Header from "$lib/Header.svelte"
+</script>
+
+<Header headingTitle="Contact us" />
+
+<form action="">
+    <label for="name">Name
+        <input type="text" id="name">
+    </label>
+    <label for="email">Email
+        <input type="email" id="email">
+    </label>
+    <label for="message">Your message</label>
+    <textarea name="message" id="meddage"></textarea>
+</form>
+
+<style>
+    form {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
