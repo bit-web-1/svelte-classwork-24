c28ec2be46a3b822ca7c395bfad8835566c28526
eallenop <e>
2024-10-22 11:20:17 +1300

    Add a news article

---
+++ src/routes/news/article/+page.svelte
@@ -0,0 +1,26 @@
+<h2>This is an article</h2>
+
+<article>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ipsam esse
+    tempora vitae nobis accusamus expedita cumque doloremque adipisci pariatur
+    necessitatibus porro fugiat, neque cum corrupti molestias veniam! Eveniet,
+    quas!
+  </p>
+  <p>
+    Praesentium excepturi maiores quam recusandae quo dolorem dolores ex enim
+    fugit quisquam, blanditiis, magnam ad perferendis exercitationem porro modi
+    incidunt distinctio vero molestiae debitis corrupti iste obcaecati. Sunt,
+    odio maxime?
+  </p>
+  <p>
+    Odit non porro quia quas corrupti similique ab quidem quae pariatur sapiente
+    eius aliquid eligendi, magni suscipit nam, accusamus perferendis facilis.
+    Ipsam labore consequatur dolorum repudiandae esse vitae asperiores et.
+  </p>
+  <p>
+    Ex saepe, laboriosam neque et soluta nesciunt tempora veritatis illo,
+    delectus, omnis vitae consectetur excepturi. Unde porro, sit dolorum illum
+    nostrum, magnam laborum delectus nisi, ducimus laudantium non harum rerum.
+  </p>
+</article>
