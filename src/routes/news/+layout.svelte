1eb15ea61e18c414b1d558eb239be0cf4a9a015c
eallenop <e>
2024-10-22 11:20:36 +1300

    Adjust the news layout to have a sidebar

---
+++ src/routes/news/+layout.svelte
@@ -0,0 +1,20 @@
+<div>
+  <aside>
+    <p>This is an aside only on the news page</p>
+    <a href="/news/article">Article</a>
+  </aside>
+  <main>
+    <slot />
+  </main>
+</div>
+
+<style>
+  div {
+    display: flex;
+  }
+  aside {
+    background-color: #eee;
+    padding: 1em;
+    margin-right: 1em;
+  }
+</style>
