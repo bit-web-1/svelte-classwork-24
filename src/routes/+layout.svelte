bd67254c5cf3cba6afaf582251626cb6ccc3bf07
eallenop <e>
2024-10-22 10:56:12 +1300

    Set all text to system font stack

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
     import Navigation from "$lib/Navigation.svelte"
 </script>
 
+<h1>I'm Learning SvelteKit</h1>
+
 <Navigation />
 
 <slot />
@@ -9,6 +11,9 @@
 <footer>This should be on every page because it's in the layout file</footer>
 
 <style>
+    :global(*) {
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
     footer {
         padding: 1em;
         background-color: #aaa;
