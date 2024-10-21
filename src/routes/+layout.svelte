a61ed9399c8d56387ac94f6bd34efdd548825334
eallenop <e>
2024-10-22 10:46:33 +1300

    Make a basic layout file

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<slot />
+
+<footer>This should be on every page because it's in the layout file</footer>
+
+<style>
+    footer {
+        padding: 1em;
+        background-color: #aaa;
+        margin-top: 1em;
+    }
+</style>
