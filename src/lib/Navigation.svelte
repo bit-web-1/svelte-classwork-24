fc654a431cd261ebe1801734e9f93d435dbedfda
eallenop <e>
2024-10-22 11:03:41 +1300

    Style the nav bar

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -5,3 +5,21 @@
         <li><a href="/contact">Contact</a></li>
     </ul>
 </nav>
+
+<style>
+    a {
+        text-decoration: none;
+        color: #333;
+        font-weight: bold;
+    }
+    li {
+        list-style: none;
+        padding: 0.5em 1em;
+        margin: 0.5em 1em;
+    }
+    ul {
+        display: flex;
+        flex-direction: row;
+        background-color: #eee;
+    }
+</style>
