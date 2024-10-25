0b6285c6e8f8a05f165a7a50d9b90684229ac625
eallenop <e>
2024-10-25 13:45:20 +1300

    Style each news item

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -6,5 +6,16 @@
 <h2>News about learning SvelteKit</h2>
 
 {#each posts as post}
+<article>
+    <h3>{post.title}</h3>
     <p>{post.body}</p>
+</article>
 {/each}
+
+<style>
+    article {
+        background-color: #dee;
+        padding: 0.5em;
+        margin-bottom: 1em;
+    }
+</style>
