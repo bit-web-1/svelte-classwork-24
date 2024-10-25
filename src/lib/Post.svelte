4ba98bdb45aef6498ce07ae40d72ac71ff39a8b2
eallenop <e>
2024-10-25 14:26:23 +1300

    Tidy up Post component code

--- src/lib/Post.svelte
+++ src/lib/Post.svelte
@@ -1,15 +1,13 @@
 <script>
   export let post; // Takes in a post object as a prop
-  let isAlert = true;
-  if (post.id > 4){isAlert=false}
+  let isAlert = post.id > 3 ? true : false; // Ternary operator (short if statement)
 </script>
 
-
-  <article class:alert={isAlert}>
-    <h3>{post.title}</h3>
-    <p>{post.body}</p>
-  </article>
-
+<!-- Apply the alert class if the id is over 3 -->
+<article class:alert={isAlert}>
+  <h3>{post.title}</h3>
+  <p>{post.body}</p>
+</article>
 
 <style>
   article {
