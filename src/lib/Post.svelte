2a8613f1367d8e2cb64fdbe185d1bf42bcf07f5c
eallenop <e>
2024-10-25 14:02:39 +1300

    Set up the post component

---
+++ src/lib/Post.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let post; // Takes in a post object as a prop
+</script>
+
+<article>
+    <h3>{post.title}</h3>
+    <p>{post.body}</p>
+</article>
+
+<style>
+    article {
+        background-color: #dee;
+        padding: 0.5em;
+        margin-bottom: 1em;
+    }
+</style>
