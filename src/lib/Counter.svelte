d27938d8c170bef0f0af137038f5d7de9e9aaf0a
eallenop <e>
2024-10-29 10:57:15 +1300

    Make an event demo component

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,10 @@
+<script>
+    let count = 0;
+
+    function incrementCount() {
+        count++;
+    }
+</script>
+
+<button on:click={incrementCount}>Add one</button>
+<p>The number of clicks is: {count}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
-    import Header from "$lib/Header.svelte"
+    import Header from "$lib/Header.svelte";
+    import Counter from "$lib/Counter.svelte";
 </script>
 
 
 <Header headingTitle="Home page" />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Counter />
